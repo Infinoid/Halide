b607b6a8d23d317b2ed46ab6b4712b6a0effd046
Tzu-Mao Li <tzumao@mit.edu>
2021-04-08 09:48:32 -0400

    fix test & remove debug prints

--- src/runtime/mpi.cpp
+++ src/runtime/mpi.cpp
@@ -22,7 +22,6 @@ WEAK bool halide_mpi_send(void *data,
                           size_t num_bytes,
                           int destination,
                           int tag) {
-    print(0) << "send, offset:" << offset << ", num_bytes:" << (int)num_bytes << ", dest:" << destination << ", data[0]:" << ((int*)data)[0] << "\n";
     MPI_Send((void*)((uint8_t*)data + offset), num_bytes, MPI_BYTE, destination, tag, MPI_COMM_WORLD);
     return true;
 }
@@ -33,7 +32,6 @@ WEAK bool halide_mpi_recv(void *data,
                           int source,
                           int tag) {
     MPI_Recv((void*)((uint8_t*)data + offset), num_bytes, MPI_BYTE, source, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-    print(0) << "recv, offset:" << offset << ", num_bytes:" << (int)num_bytes << ", src:" << source << ", data[0]:" << ((int*)data)[0] << "\n";
     return true;
 }
 
